72a5ad88084129c8171d92348dd305263964a6db
ABCarrigan <addisonbcarrigan@gmail.com>
2022-03-08 14:55:12 -0500

    updates for heroku

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const PORT = process.env.PORT || 3000
 
 app.set('view engine', 'jsx');
 app.engine('jsx', require('express-react-views').createEngine());
@@ -34,6 +35,6 @@ app.post('/fruits/', (req, res)=>{
     fruits.push(req.body);
 });
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
     console.log('listening');
 });
